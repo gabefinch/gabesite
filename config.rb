57ec7cbaa5b0faffbdca743478d090910bef44bb
Gabe Finch <gabefinch@gmail.com>
2015-08-15 11:02:10 -0700

    remove /home from asset paths in config, remove relative assets true from config

--- config.rb
+++ config.rb
@@ -47,9 +47,9 @@ end
 #   end
 # end
 
-set :css_dir, 'home/stylesheets'
+set :css_dir, 'stylesheets'
 
-set :js_dir, 'home/javascripts'
+set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
@@ -65,9 +65,9 @@ configure :build do
   # Enable cache buster
   # activate :asset_hash
 
-  Use relative URLs
+  # Use relative URLs
   activate :relative_assets
-  set :relative_links, true
+  # set :relative_links, true
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
